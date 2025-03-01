c1be337668745a00bb205213e013b8ef4f1835d1
nodominio <dominiosemfundo@gmail.com>
2025-02-28 21:35:54 -0400

    Fiz nada não

---
+++ services/compromisso_services.py
@@ -0,0 +1,10 @@
+import db
+
+def cadastrar_compromisso():
+    nome_empresa = input("Digite o nome_empresa: ") 
+    cnpj = input("Digite o cnpj: ")
+    observacao = input("Digite a observacao: ")
+    contato = input("Digite o contato: ")
+    endereco = input("Digite o endereco: ")
+    resposta = db.insere_cliente(nome_empresa, cnpj,observacao,contato, endereco)
+    return resposta
