ff03ebc92bb2402107f47ea8a53f7f64046c1fd1
Kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-03-01 00:44:38 -0300

    listagem de dados

--- db.py
+++ db.py
@@ -23,3 +23,13 @@ def altera_dados_cliente(tabela, coluna, alteracao, id):
         return "Dados do clientes não alterados"
     
     
+def lista_clientes(colunas_select):
+    try:
+        cursor.execute(f'''SELECT {colunas_select} FROM cliente''')
+        linhas = cursor.fetchall()
+        for linha in linhas:
+            print(linha)
+        
+        return "Select deu certo"
+    except Exception as e:
+        return f"Não deu certo : {e}"
