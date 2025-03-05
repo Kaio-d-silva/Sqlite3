7907a4bfad57d6a5719bb3c86128e59dc143d1b2
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-03-04 23:10:27 -0300

    limpeza

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from services.clientes_services import *
 from auxiliares.menu_main import menu_opcoes
 
-# # status = listar_clientes("observacao", "cnpj")
-# status = inativar_cliente(1)
-# print(status)
-# # cadastrar_cliente()
-
+# Inicio programa
 if __name__ == "__main__":
     resposta = menu_opcoes()
     print(resposta)
