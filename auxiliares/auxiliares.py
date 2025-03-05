9ece0f8f1044d80dc71f8e79b361f1355e2c7770
root <root@DESKTOP-5FBQO9E.>
2025-03-04 23:03:31 -0300

    funcao de listar opcoes, essa funcao recebe uma lista das opcoes e faz um laço para printar cada uma enumerada

--- auxiliares/auxiliares.py
+++ auxiliares/auxiliares.py
@@ -1,2 +1,10 @@
 def marcacao_linha():
     print(50 * "-")
+    
+def lista_opcoes(opcoes):
+    for opcao in opcoes:
+        enumerador = opcao
+        # Caso tenha _ ele retira.
+        nome_funcao = opcoes[enumerador].replace("_"," ")
+        print(f"{enumerador} {nome_funcao}")
+    
