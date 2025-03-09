9a8e0d770a004faf7eb0eccf975474fe6e9880b8
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-03-08 21:08:44 -0400

    fiz um whilw true para mander a aplicação rodando ate que a pessoa peça para siar

--- auxiliares/menu_main.py
+++ auxiliares/menu_main.py
@@ -1,24 +1,32 @@
 from services.clientes_services import *
 from services.contas_a_receber_services import *
-from auxiliares.auxiliares import marcacao_linha, lista_opcoes
+from auxiliares.auxiliares import marcacao_linha, lista_opcoes, limpar_terminal
 
 def menu_opcoes():
     # tabelas = ["Clientes", "Agenda Compromisso","Contas a Receber" ]
     
-    tabelas = {
+    opcoes = {
         1 : "Clientes",
         2 : "Agenda Compromisso",
-        3 : "Contas a Receber"
+        3 : "Contas a Receber",
+        4 : "Sair"
     }
     
-    
-    print("Oque deseja acessar ?")
-    lista_opcoes(tabelas)
-    
-    tabela_selecionada = int(input("Numero escolido : "))
-    marcacao_linha()
-    resposta =  analisa_escolha(tabelas,tabela_selecionada)
-    return resposta
+    while True:
+        limpar_terminal()
+        print("****     Menu Opções     ****\n")
+        lista_opcoes(opcoes)
+
+        opcao_selecionada = int(input("Numero escolido : "))
+        marcacao_linha()
+        
+        resposta =  analisa_escolha(opcoes,opcao_selecionada)
+        
+        print(resposta)
+        marcacao_linha()
+        if resposta == "sair":
+            break
+    # return resposta
 
 def analisa_escolha(tabelas,escolha):
     if escolha in tabelas:
@@ -29,9 +37,12 @@ def analisa_escolha(tabelas,escolha):
                 resposta = opcoes_agenda_compromisso()
             case 3: # Contas a Receber
                 resposta = opcoes_contas_a_receber()
+            case 4: # Sair
+                resposta = "sair"
         
         return resposta
     else:
+        marcacao_linha()
         return "Escolha não permitida"
         
         
@@ -40,60 +51,73 @@ def opcoes_cliente():
         1 :"cadastrar_cliente",
         2 :"alterar_cadastro",
         3 :"listar_clientes",
-        4 :"inativar_cliente"    
+        4 :"inativar_cliente",
+        5 : "voltar"   
     }
     
-    print("Oque deseja fazer : ")
-    lista_opcoes(funcoes_cliente)
-    
-    funcao_escolhida = int(input("Escolha o numero da função : "))
-    marcacao_linha()
-    match funcao_escolhida:
-        case 1: # Cadastro cliente
-            resposta = cadastrar_cliente()
-        case 2: #Alterar cadastro
-            cliente_id = int(input("Qual o ID do cliente : "))
-            resposta = alterar_cadastro(cliente_id)
-        case 3: #Listar clientes
-            # Colocar logica para ver quais colunas seram alteradas
-            resposta = listar_clientes("*")
-        case 4: #Inativar cliente
-            cliente_id = int(input("Qual o ID do cliente : "))
-            resposta = inativar_cliente(cliente_id)
-    return resposta        
+    while True:
+        limpar_terminal()
+        print("****     Escolha uma Opção     ****\n")
+        lista_opcoes(funcoes_cliente)
+        
+        funcao_escolhida = int(input("Escolha o numero da função : "))
+        marcacao_linha()
+        match funcao_escolhida:
+            case 1: # Cadastro cliente
+                resposta = cadastrar_cliente()
+            case 2: #Alterar cadastro
+                cliente_id = int(input("Qual o ID do cliente : "))
+                resposta = alterar_cadastro(cliente_id)
+            case 3: #Listar clientes
+                # Colocar logica para ver quais colunas seram alteradas
+                resposta = listar_clientes("*")
+            case 4: #Inativar cliente
+                cliente_id = int(input("Qual o ID do cliente : "))
+                resposta = inativar_cliente(cliente_id)
+            case 5:
+                return "saindo opcoes cadastro"
+        print(resposta)
+        marcacao_linha()
+        
+           
 
 # Coloque suas funções aqui dentro, e siga o padrão do match case da função "opcoes clientes"
 def opcoes_agenda_compromisso():
+    limpar_terminal()
     funcoes_agenda = {
 
     }            
     
 def opcoes_contas_a_receber():
     funcoes_contas_a_receber = {
-        1:"lancar_conta_a_receber",
-        2:"alterar_lancamento",
-        3:"exibir_lancamentos",
-        4:"inativar_lancamento"
+        1:"lancar conta a receber",
+        2:"alterar lancamento",
+        3:"exibir lancamentos",
+        4:"inativar lancamento",
+        5:"Voltar"
     }
     
-    print("Oque deseja fazer : ")
-    lista_opcoes(funcoes_contas_a_receber)
-    
-    funcao_escolhida = int(input("Escolha o numero da função : "))
-    marcacao_linha()
-    match funcao_escolhida:
-        case 1: # Lancar conta a receber
-            resposta = lancar_conta_a_receber()
-        case 2: # alterar_lancamento
-            lancamento_id = int(input("Qual o ID do lancamento : "))
-            resposta = alterar_lancamento(lancamento_id)
-        case 3: # listar_lancamentos
-            resposta = listar_lancamentos("*")
-        case 4: # Inativar lançamento
-            lancamento_id = int(input("Qual o ID do lancamento : "))
-            resposta = inativar_lancamento(lancamento_id)
+    while True:
+        limpar_terminal()
+        print("****     Escolha uma Opção     ****\n")
+        lista_opcoes(funcoes_contas_a_receber)
+        
+        funcao_escolhida = int(input("Escolha o numero da função : "))
+        marcacao_linha()
+        match funcao_escolhida:
+            case 1: # Lancar conta a receber
+                resposta = lancar_conta_a_receber()
+            case 2: # alterar_lancamento
+                lancamento_id = int(input("Qual o ID do lancamento : "))
+                resposta = alterar_lancamento(lancamento_id)
+            case 3: # listar_lancamentos
+                resposta = listar_lancamentos("*")
+            case 4: # Inativar lançamento
+                lancamento_id = int(input("Qual o ID do lancamento : "))
+                resposta = inativar_lancamento(lancamento_id)
+            case 5: # Voltar
+                return "Saindo opções conta a receber"                
+        print(resposta)    
+        marcacao_linha()
 
-            
-    return resposta    
-    
     
