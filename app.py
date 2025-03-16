54306d47ab368bcdd6b7cab2013ff20715c7d8e6
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-03-15 21:37:51 -0400

    limpeza

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from services.clientes_services import *
 from auxiliares.menu_main import menu_opcoes
+from services.clientes_services import listar_clientes
 
 # Inicio programa
 if __name__ == "__main__":
     menu_opcoes()
-    # resposta = menu_opcoes()
-    # print(resposta)
