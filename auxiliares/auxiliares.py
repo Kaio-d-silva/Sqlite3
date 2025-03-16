e7528cfebfbc44b581993814c0360d8f0179e425
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-03-15 21:37:32 -0400

    padronização do codigo, inserção do compromisso no menu, correção de algumas funções

--- auxiliares/auxiliares.py
+++ auxiliares/auxiliares.py
@@ -1,4 +1,5 @@
 import os
+from db import listar_dados
 
 def marcacao_linha():
     print(40 * "-")
@@ -17,3 +18,16 @@ def limpar_terminal():
     else:
         os.system("clear")
         
+
+
+# Função que tem a finalidade de fazer um filtro no select, nao esta em uso atualmente
+def filtro_listar_dados(nome_tabela, *nome_coluna):
+    '''Escolha as colunas que deseja filtrar ou use não passe nenhuma para ter todos os dados'''
+    # Se nenhuma coluna for passada como argumento ele usa o valor * no select
+    if not nome_coluna:
+        nome_coluna = "*"
+    
+    valor_de_busca = ",".join(nome_coluna)
+    resposta = listar_dados(valor_de_busca, nome_tabela)
+    marcacao_linha()
+    return resposta
--- auxiliares/menu_main.py
+++ auxiliares/menu_main.py
@@ -63,29 +63,31 @@ def opcoes_cliente():
         print("****     Escolha uma Opção     ****\n")
         lista_opcoes(funcoes_cliente)
         
-        funcao_escolhida = input("Escolha o numero da função : ")
-        marcacao_linha()
         try:
-            escolha_formatada = int(funcao_escolhida)
-            match escolha_formatada:
+            funcao_escolhida = int(input("Escolha o numero da função : "))
+            marcacao_linha()
+        
+            match funcao_escolhida:
                 case 1: # Cadastro cliente
                     resposta = cadastrar_cliente()
                 case 2: #Alterar cadastro
-                    cliente_id = int(input("Qual o ID do cliente : "))
-                    resposta = alterar_cadastro(cliente_id)
+                    resposta = alterar_cadastro()
                 case 3: #Listar clientes
                     # Colocar logica para ver quais colunas seram alteradas
-                    resposta = listar_clientes("*")
+                    resposta = listar_clientes()
                 case 4: #Inativar cliente
-                    cliente_id = int(input("Qual o ID do cliente : "))
-                    resposta = inativar_cliente(cliente_id)
+                    resposta = inativar_cliente()
                 case 5:
                     return "saindo opcoes cadastro"
             print(resposta)
             marcacao_linha()
-            input("De um enter para continuar ")
         except:
-            pass
+            marcacao_linha()
+            print("Opção invalida")
+            marcacao_linha()
+            
+        input("De um enter para continuar ")
+            
         
            
 
@@ -94,26 +96,42 @@ def opcoes_compromisso():
     funcoes_compromisso = {
         1:"Cadastrar compromisso",
         2:"Alterar compromisso",
-        3:"Exibir compromisso",
-        4:"Ocultar compromisso"
+        3:"Exibir compromissos",
+        4:"Ocultar compromisso",
+        5:"voltar"
     }
 
-    print("O que deseja fazer: ")
-    lista_opcoes(funcoes_compromisso)
-
-    funcao_escolhida = int(input("Escolha uma opção:"))
-    marcacao_linha()
-    match funcao_escolhida:
-        case 1:
-            resposta = cadastrar_compromisso()
-        case 2:
-            resposta = alterar_compromisso()
-        case 3:
-            resposta = exibir_mensagem()
-        case 4:
-            resposta = inativar_compromisso()         
-    return resposta 
     
+    while True:
+        limpar_terminal()
+        print("****     Escolha uma Opção     ****\n")
+        lista_opcoes(funcoes_compromisso)
+
+        try:
+            funcao_escolhida = int(input("Escolha o numero da função : "))
+            marcacao_linha()
+        
+            match funcao_escolhida:
+                case 1:
+                    resposta = cadastrar_compromisso()
+                case 2:
+                    resposta = alterar_compromisso()
+                case 3:
+                    resposta = listar_compromissos()
+                case 4:
+                    resposta = inativar_compromisso()    
+                case 5:     
+                    return "Saindo de opcoes compromisso"
+            print(resposta) 
+            marcacao_linha()
+        except:
+            marcacao_linha()
+            print("Opção invalida")
+            marcacao_linha()
+        
+        input("De um enter para continuar ")
+
+            
 def opcoes_contas_a_receber():
     funcoes_contas_a_receber = {
         1:"lancar conta a receber",
@@ -128,23 +146,25 @@ def opcoes_contas_a_receber():
         print("****     Escolha uma Opção     ****\n")
         lista_opcoes(funcoes_contas_a_receber)
         
-        funcao_escolhida = int(input("Escolha o numero da função : "))
-        marcacao_linha()
-        match funcao_escolhida:
-            case 1: # Lancar conta a receber
-                resposta = lancar_conta_a_receber()
-            case 2: # alterar_lancamento
-                lancamento_id = int(input("Qual o ID do lancamento : "))
-                resposta = alterar_lancamento(lancamento_id)
-            case 3: # listar_lancamentos
-                resposta = listar_lancamentos("*")
-            case 4: # Inativar lançamento
-                lancamento_id = int(input("Qual o ID do lancamento : "))
-                resposta = inativar_lancamento(lancamento_id)
-            case 5: # Voltar
-                return "Saindo opções conta a receber"                
-        print(resposta)    
-        marcacao_linha()
+        try:            
+            funcao_escolhida = int(input("Escolha o numero da função : "))
+            marcacao_linha()
+            
+            match funcao_escolhida:
+                case 1: # Lancar conta a receber
+                    resposta = lancar_conta_a_receber()
+                case 2: # alterar_lancamento
+                    resposta = alterar_lancamento()
+                case 3: # listar_lancamentos
+                    resposta = listar_lancamentos()
+                case 4: # Inativar lançamento
+                    resposta = inativar_lancamento()
+                case 5: # Voltar
+                    return "Saindo opções conta a receber"                
+            print(resposta)    
+            marcacao_linha()
+        except:
+            print("Opção invalida")
         input("De um enter para continuar ")
 
     
